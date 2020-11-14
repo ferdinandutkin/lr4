cc75ef2042cae0657fdc6c3eafcf1ff19c72e7eb
ferdinandutkin <ferdinandutkin@gmail.com>
2020-11-14 14:32:18 +0300

    Добавьте файлы проекта.

---
+++ Set/Owner.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace LR4
+{
+    public partial class Set<T>
+    {
+
+        public static Owner OwnerInfo { get; private set; }
+        public static Date CreationDate { get; private set; }
+
+        static Set()
+        {
+            OwnerInfo = new Owner("61806555", "Stanislav Tumash", "BSTU");
+            CreationDate = "21.09.2020";
+        }
+
+
+        public class Date
+        {
+            private ushort day, month;
+            public ushort Day
+            {
+                get => day;
+                set
+                {
+                    if (value > 31)
+                    {
+                        day = 31;
+                    }
+                    else
+                    {
+                        day = value;
+                    }
+                }
+            }
+            public ushort Month
+            {
+                get => month;
+                set
+                {
+                    if (value > 12)
+                    {
+                        month = 12;
+                    }
+                    else
+                    {
+                        month = value;
+                    }
+                }
+            }
+            public int Year { get; set; }
+
+
+            public Date(string date)
+            {
+                string[] tokens = date.Split('.', StringSplitOptions.RemoveEmptyEntries);
+                if (tokens.Length != 3)
+                {
+                    Debug.WriteLine("парсинг даты сломался");
+                }
+                else
+                {
+                    Day = ushort.Parse(tokens[0]);
+                    Month = ushort.Parse(tokens[1]);
+                    Year = int.Parse(tokens[2]);
+
+                }
+
+            }
+
+            public Date(ushort day, ushort month, int year)
+            {
+                Day = day;
+                Month = month;
+                Year = year;
+            }
+
+
+            public static implicit operator Date(string s) => new Date(s);
+
+            public override string ToString() => $"{Day}.{Month}.{Year}";
+
+            public override bool Equals(object o)
+            {
+
+                if ((o == null) || !GetType().Equals(o.GetType()))
+                {
+                    return false;
+                }
+                else
+                {
+                    var d = (Date)o;
+                    return (d.Day == Day) && (d.Month == Month) && (d.Year == Year);
+                }
+            }
+
+            public override int GetHashCode() => HashCode.Combine(Day, Month, Year);
+        }
+
+        public class Owner
+        {
+            public string GithubID { get; private set; }
+            public string Name { get; private set; }
+            public string Organization { get; private set; }
+            public Owner(string id, string name, string organisation)
+            {
+
+                GithubID = id;// "61806555";
+                Name = name;// "Tumash Stanislav";
+                Organization = organisation;// "BSTU";
+            }
+
+            public override string ToString() => $"ID: {GithubID}, Имя: {Name}, Организация: {Organization}";
+          
+        }
+
+    }
+   
+}
---
+++ Set/Program.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+ 
+namespace LR4
+{
+    public static class StatisticalOperations
+    {
+        public static Set<T> NormalizedButExtension<T>(this Set<T> s)
+        {
+            s.Normalize();
+            return s;
+        }
+        public static string CommaAfterEachWord(this string s) => string.Join(", ", s.Split(' ', StringSplitOptions.RemoveEmptyEntries)) + ", ";
+
+        public static int SumButExtension(this Set<int> s) => s.Sum();
+
+        public static int MinMaxDifference(this Set<int> s) => s.Min() - s.Max();
+
+        public static int Count(this Set<int> s) => (int)s;
+
+
+        public static bool IsWithinRange(this Set<int> s, int[] arr, int top, int bottom) => bottom <= arr.Length && arr.Length <= top;
+
+
+
+
+    }
+
+
+
+    public partial class Set<T> : IEnumerable<T>
+    {
+     
+
+    List<T> container;
+
+        public Set(params T[] values)
+        {
+            container = values.ToList();
+            Normalize();
+        }
+
+
+        public Set(IEnumerable<T> values)
+        {
+            container = values.ToList();
+            Normalize();
+        }
+
+
+
+        // public static bool operator >=(Set<T> l, Set<T> r) => true;
+        // public static bool operator <=(Set<T> l, Set<T> r) => true; 
+ 
+        public T this[int index]
+        {
+            get => container[index];
+            set => container[index] = value;              
+        }
+
+        public void Add(T el) => container.Add(el);
+      
+        public void Normalize() => container = this.Distinct().ToList(); 
+        public static Set<T> operator +(Set<T> l, Set<T> r) => new Set<T>(l.Concat(r));
+        public static Set<T> operator +(Set<T> s, T el) => new Set<T>(s.Append(el));
+        public static Set<T> operator +(T el, Set<T> s) => new Set<T>(s.Prepend(el));
+
+        public static Set<T> operator *(Set<T> l, Set<T> r) => new Set<T>(l.Intersect(r));
+
+        public static explicit operator int(Set<T> s) => s.container.Count;
+
+      
+       
+        public IEnumerator<T> GetEnumerator() => ((IEnumerable<T>)container).GetEnumerator();
+       
+        IEnumerator IEnumerable.GetEnumerator() => container.GetEnumerator();
+
+        public override string ToString() => '{' + string.Join(", ", this) + '}';
+         
+
+
+    }
+
+    
+    class Program
+    {
+
+        static void Main()
+        {
+            var set1 = new Set<int> { 1, 2, 5, 2, 13, 2, 4 };
+            var set2 = new Set<int> { 7, 6, 5, 2, 4, 1 };
+
+          
+
+            Console.WriteLine($"{set1} + {set2} = {set1 + set2}");
+
+            Console.WriteLine($"{set1} * {set2} = {set1 * set2}");
+            
+            Console.WriteLine($"{set1} + 12 = {set1 + 12}");
+
+            Console.WriteLine($"12 + {set1} = {12 + set1}"); 
+
+            Console.WriteLine($"(int){set1} = {(int)set1}");
+
+
+            Console.WriteLine(Set<int>.OwnerInfo);
+
+            Console.WriteLine("тестовая строка".CommaAfterEachWord());
+
+            Console.WriteLine(new Set<int>(1, 3, 4, 3, 3, 1).NormalizedButExtension());
+
+
+
+        }
+    }
+}
